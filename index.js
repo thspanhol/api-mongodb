d1911598fe934f26ba305f51376564e1d1277d81
Thales Spanhol <thales_spanhol@hotmail.com>
2023-07-12 20:51:06 -0300

    Refactor: refatoração do index

--- index.js
+++ index.js
@@ -1,21 +1,20 @@
 // config inicial
-const express = require('express')
-const app = express()
+const express = require("express");
+const app = express();
 
 // forma de ler JSON / middlewares
 app.use(
-    express.urlencoded({
-        extended: true,
-    }),
-)
+  express.urlencoded({
+    extended: true,
+  })
+);
 
-app.use(express.json())
+app.use(express.json());
 
 // rota inicial / endpoint
-app.get('/', (req, res) => {
-    res.json({message: 'Oi Express!'})
-} )
-
+app.get("/", (req, res) => {
+  res.json({ message: "Oi Express!" });
+});
 
 // entregar uma porta
-app.listen(3000)
+app.listen(3000);
