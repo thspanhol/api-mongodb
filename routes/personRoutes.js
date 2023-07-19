fd688acc00960f633f4e9e5ecd09ef7ce95e0616
Thales Spanhol <thales_spanhol@hotmail.com>
2023-07-19 17:15:59 -0300

    Feat: rota update

--- routes/personRoutes.js
+++ routes/personRoutes.js
@@ -52,7 +52,10 @@ router.post('/', async (req, res) => {
 
         const person = await Person.findOne({_id: id})
 
-        if (!person) res.status(422).json({message: 'O usuário não foi encontrado'})
+        if (!person) {
+            res.status(422).json({message: 'O usuário não foi encontrado'})
+            return
+        }
 
         res.status(200).json(person)
 
@@ -61,5 +64,35 @@ router.post('/', async (req, res) => {
       }
 
   })
+
+  // Update
+router.patch('/:id', async(req, res) => {
+
+    const id = req.params.id
+
+    const {name, salary, approved} = req.body
+
+    const person = {
+        name,
+        salary,
+        approved
+      }
+
+      try {
+
+        const updatedPerson = await Person.updateOne({_id: id}, person)
+
+        if(updatedPerson.matchedCount === 0) {
+            res.status(422).json({message: 'O usuário não foi encontrado'})
+            return
+        }
+
+        res.status(200).json(person)
+
+    } catch (error) {
+        res.status(500).json({error: error})
+      }
+
+})
 
   module.exports = router;
