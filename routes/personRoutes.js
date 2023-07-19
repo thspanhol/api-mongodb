12b61ec707205ec443c4d2d8a7d5fdfa998b6ce7
Thales Spanhol <thales_spanhol@hotmail.com>
2023-07-19 16:55:53 -0300

    Feat: rota read

--- routes/personRoutes.js
+++ routes/personRoutes.js
@@ -9,7 +9,8 @@ router.post('/', async (req, res) => {
   
     if(!name || !salary || approved === undefined) {
       res.status(422).json({error: 'Não cumpriu com os dados obrigatórios!'})
-    } else {
+      return
+    }
         const person = {
             name,
             salary,
@@ -24,8 +25,41 @@ router.post('/', async (req, res) => {
           } catch (error) {
             res.status(500).json({error: error})
           }
-    }
+    
   
+  })
+
+  // Read
+  router.get('/', async (req, res) => {
+
+    try {
+
+        const people = await Person.find()
+
+        res.status(200).json(people)
+
+    } catch (error) {
+        res.status(500).json({error: error})
+      }
+
+  })
+
+  router.get('/:id', async (req, res) => {
+
+    const id = req.params.id
+
+    try {
+
+        const person = await Person.findOne({_id: id})
+
+        if (!person) res.status(422).json({message: 'O usuário não foi encontrado'})
+
+        res.status(200).json(person)
+
+    } catch (error) {
+        res.status(500).json({error: error})
+      }
+
   })
 
   module.exports = router;
