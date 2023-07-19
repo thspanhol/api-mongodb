2de0921c8cd14d8945bb50f645b0c4929af42db3
Thales Spanhol <thales_spanhol@hotmail.com>
2023-07-19 17:33:41 -0300

    Feat: rota delete

--- routes/personRoutes.js
+++ routes/personRoutes.js
@@ -95,4 +95,28 @@ router.patch('/:id', async(req, res) => {
 
 })
 
+// Delete
+router.delete('/:id', async (req, res) => {
+
+    const id = req.params.id
+
+    const person = await Person.findOne({_id: id})
+
+    if (!person) {
+        res.status(422).json({message: 'O usuário não foi encontrado!'})
+        return
+    }
+
+    try {
+
+        await Person.deleteOne({_id: id})
+
+        res.status(200).json({message: 'Usuário removido com sucesso!'})
+
+    } catch (error) {
+        res.status(500).json({error: error})
+      }
+
+})
+
   module.exports = router;
